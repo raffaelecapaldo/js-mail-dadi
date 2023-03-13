91dea6e11693a961335dcee313f1266215c714be
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-13 17:39:42 +0100

    Added dice game UI

--- js/mail.js
+++ js/mail.js
@@ -8,6 +8,7 @@ const buttonMail = document.getElementById("button-mail");
 
 
 buttonMail.addEventListener("click", function () {
+    const boxAlert = document.getElementById("alert");
     const mailList = ["raffaelecapaldo@alice.it", "samuelpanicucci@gmail.com", "cleliafradella@libero.it", "marcoacciari@boolean.careers"];
     let userMail = document.getElementById("user-mail").value;
     let mailFinded;
@@ -19,14 +20,20 @@ buttonMail.addEventListener("click", function () {
 
     if (mailFinded) {
         console.log("Mail trovata");
+        boxAlert.innerHTML = `<p class="green text-center">Sei nella VIP list</p>`
+        
     }
 
     else if (!userMail.includes("@" || userMail.includes("."))) {
         console.log("Mail non valida");
+        boxAlert.innerHTML = `<p class="red text-center">Mail non valida</p>`
+
     }
 
     else {
         console.log("Mail non presente");
+        boxAlert.innerHTML = `<p class="red text-center">Non sei nella VIP list</p>`
+
     }
 
 })
