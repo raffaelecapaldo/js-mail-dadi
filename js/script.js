019be49458c26d5a5c65c0667a143b883232eb05
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-13 16:45:34 +0100

    Added check if the mail is finded

--- js/script.js
+++ js/script.js
@@ -15,23 +15,18 @@ const mailList = ["raffaelecapaldo@alice.it", "samuelpanicucci@gmail.com", "clel
 
 buttonMail.addEventListener("click", function() {
     let userMail = document.getElementById("user-mail").value;
-    let mailOk;     
-    for (let i = 0; i <= mailList.length - 1 ; i++) { //CICLO PER CONFRONTARE OGNI INDICE DELLA LISTA
-        console.log(userMail);
-        console.log(mailList[i]);
-mailOk = (!userMail.includes("@") || !userMail.includes(".")) ? "notmail" : (userMail == mailList[i]) ? "true":"false"; //Se non c'è la chiocciola in userMail, mailOk diventa notmail, altrimenti viene comparato con la lista, se è presente diventa true
+    let mailFinded;     
+   for (i = 0; i <= mailList.length; i++) {
+    if (userMail == mailList[i]) {
+        mailFinded = mailList[i]; 
+    }}
+   
+    if (mailFinded) {
+        console.log ("Mail trovata")
     }
 
-    console.log(mailOk);
-
-if (mailOk == "notmail") { //Se mailOk è notmail, messaggio di errore NO MAIL
-    console.log("Formato mail non corretto");
-}
-
-else if (mailOk == "true") { //Se mailOk è true, messaggio presente nella lista
-    console.log("Mail presente");
-}
-else  // Di conseguenza, se le due sono false, messaggio la mail non è presente nella lista
-    console.log("Mail non presente");
+    else {
+        console.log("Mail non trovata")
+    }
 
 })
